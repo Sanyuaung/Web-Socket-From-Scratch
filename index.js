149c2db1e3365cd6c77baffdde6b0f6af18377fe
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2021-09-05 21:31:06 +0630

    Lesson 2

---
+++ index.js
@@ -0,0 +1,16 @@
+let express=require('express')
+
+/**---app setup--*/
+let app=express()
+
+
+/**---server setup--*/
+let server=app.listen(4000,()=>{
+    console.log('Project is running on localhost:4000')
+});
+
+
+/**---route setup--*/
+app.get('/',(res,req)=>{
+    req.sendFile(__dirname + "/public/index.html")
+});
